630410f2e8fa8c1c9ede534232a188eb709f1c38
CrePavan <93823376+CrePavan@users.noreply.github.com>
2021-11-28 14:22:10 +0530

    Update userbotjoin.py

--- program/userbotjoin.py
+++ program/userbotjoin.py
@@ -35,7 +35,7 @@ async def join_group(client, message):
         print(e)
         await message.reply_text(
             f"🛑 𝗙𝗹𝗼𝗼𝗱 𝗪𝗮𝗶𝘁 𝗘𝗿𝗿𝗼𝗿 🛑 \n\n**ᴄʀᴇᴀᴛᴏʀ ᴘᴀᴠᴀɴ ᴏᴘ ᴜꜱᴇʀʙᴏᴛ ᴄᴀɴ'ᴛ ᴊᴏɪɴ ᴜʀ ɢʀᴏᴜᴘ ᴅᴜᴇ ᴛᴏ ʜᴇᴀᴠʏ ᴊᴏɪɴ ʀᴇQᴜᴇꜱᴛ.**"
-            "\n\n**ᴀᴅᴅ ᴀꜱꜱɪꜱᴛᴀɴᴛ ᴍᴀɴᴜᴀʟʟʏ ɪɴ ᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ.**\n\n**ᴜꜱᴇʀʙᴏᴛ : ** @CrepanAssistant\n\n**ᴏʀ ᴍꜱɢ ᴀᴛ : [𝗦𝘂𝗽𝗽𝗼𝗿𝘁 𝗚𝗿𝗼𝘂𝗽](https://t.me/CreatorPavanSupport)**\n\n**ᴏʀ ᴄᴏɴᴛᴀᴄᴛ ᴏᴘ ᴍᴀꜱᴛᴇʀ ᴄʀᴇᴀᴛᴏʀ ᴘᴀᴠᴀɴ**",
+            "\n\n**ᴀᴅᴅ ᴀꜱꜱɪꜱᴛᴀɴᴛ ᴍᴀɴᴜᴀʟʟʏ ɪɴ ᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ.**\n\n**ᴜꜱᴇʀʙᴏᴛ : ** @CrepanAssistant\n\n**ᴍꜱɢ ᴀᴛ : @CreatorPavanSupport**\n\n**ᴏʀ ᴄᴏɴᴛᴀᴄᴛ ᴏᴘ ᴍᴀꜱᴛᴇʀ ᴄʀᴇᴀᴛᴏʀ ᴘᴀᴠᴀɴ.**",
         )
         return
     await message.reply_text(
